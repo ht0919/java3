1e26fd75b97b24c3a76ae0a14f0db48e0c4275c2
Hiroyuki Tanaka <ht0919@gmail.com>
2016-11-18 16:21:48 +0900

    No.5

--- 05_1118/Li0701.java
+++ 05_1118/Li0701.java
@@ -1,7 +1,7 @@
 public class Li0701 {
     public static void main(String[] args) {
         /* 何らかのデータを読み込む処理 */
-        if( true /* データが正しくないなら */) {
+        if( false /* データが正しくないなら */) {
             System.out.println("データが壊れています。異常終了します。");
             System.exit(1);     // ここでJVMを異常終了
         }
